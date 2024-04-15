565510a27a684fcafb01add869ec6bee1985db90
Squilto <edwardouston@shaw.ca>
2024-04-14 22:03:02 -0600

    Added footer and header usage

---
+++ app/Components/Footer.js
@@ -0,0 +1,11 @@
+"use client"
+
+const Footer = () => {
+    return (
+        <div className="bg-#836554 py-3">
+
+        </div>
+    );
+};
+
+export default Footer;
---
+++ app/Components/Header.js
@@ -0,0 +1,11 @@
+"use client"
+
+const Header = () => {
+    return (
+        <div className="bg-#836554 py-3">
+
+        </div>
+    );
+};
+
+export default Header;
