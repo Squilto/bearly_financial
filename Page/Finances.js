12d3dabcd97d7db34f8159191327b42d0b34e432
Squilto <edwardouston@shaw.ca>
2024-04-15 01:51:06 -0600

    Restructuring

---
+++ Page/Finances.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+
+const Finances = ({ finances, onDelete }) => {
+ 
+
+    return (
+        <Layout>
+            
+        </Layout>
+    )
+}
--- app/Components/Header.js
+++ app/Components/Header.js
@@ -2,8 +2,22 @@
 
 const Header = () => {
     return (
-        <div className="bg-#836554 py-3">
-
+        <div className="bg-GoldenRod py-3">
+            <ul>
+                <li>
+                    <Link legacyBehavior href="/">
+                    <a>
+                        <img src="public\bear-icon-vector.jpg" alt="LogoIcon"></img>
+                    </a>
+                    </Link>
+                </li>
+                    
+                <li>
+                    <Link>
+                    <a>Financial Calculator</a>
+                    </Link>
+                </li>
+            </ul>
         </div>
     );
 };
