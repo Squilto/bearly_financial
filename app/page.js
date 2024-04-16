2d2a80ddd6fd86235ddb3720d9a423037d499b98
hagenchung2 <hagen.chung@edu.sait.ca>
2024-04-15 19:28:00 -0600

    added login requirement to main page

--- app/page.js
+++ app/page.js
@@ -1,13 +1,45 @@
+'use client'
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase/config";
+import { signOut } from 'firebase/auth';
+
+export function ForceLog() {
+  const [user] = useAuthState(auth);
+  const router = useRouter()
+  const userSession = sessionStorage.getItem('user');
+
+  console.log(userSession)
+ 
+  if (!user && !userSession){
+    router.push('/sign-in')
+  }
+  
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button onClick={() => {
+        signOut(auth)
+        sessionStorage.removeItem('user')
+        router.push('/sign-in')
+        }}>
+        Log out
+      </button>
+    </main>
+  )
+}
 
 const Home = ({children}) => {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header></Header>
       <div>
         {children}
       </div>
+      <ForceLog></ForceLog>
       <Footer></Footer>
     </main>
   );
