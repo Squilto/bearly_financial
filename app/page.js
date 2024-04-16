a522c3104046bde7a900cdfa27c73cd5a90b49df
hagenchung2 <hagen.chung@edu.sait.ca>
2024-04-15 23:38:48 -0600

    making page.js redirect to signin if not logged

--- app/page.js
+++ app/page.js
@@ -6,17 +6,20 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase/config";
 import { signOut } from 'firebase/auth';
 
+
 const Home = ({children}) => {
   const [user] = useAuthState(auth);
   const router = useRouter()
   const userSession = sessionStorage.getItem('user');
 
   console.log(userSession)
- 
+   if (!user && !userSession){
+    router.push('/sign-in')
+  }
   return (
-    <main className="flex min-h-screen flex-col m-auto items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header></Header>
-      <div className="bg-yellow-800 max-m flex-col justify-between flex-center">
+      <div>
         {children}
       </div>
       <button onClick={() => {
